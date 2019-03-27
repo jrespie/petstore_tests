637079ce58217d62ed1d79a37f4c2f38fb723400
James Espie <james.espie@pushpay.com>
2019-03-27 20:11:26 +1300

    Add some assertions on pet name

--- test_petstore.py
+++ test_petstore.py
@@ -10,8 +10,10 @@ pet_name = fake.name()
 def test_create_pet():
 	response=petstore_queries.create_pet(pet_id,pet_name)
 	assert response.status_code==200, "Expected status 200 but received {}".format(response.status_code)
+	assert response.name==pet_name, "Expected pet name to be {} but received {}".format(pet_name,response.name)
 
 def test_get_pet():
 	response=petstore_queries.get_pet_by_id(pet_id)
 	assert response.status_code==200, "Expected status 200 but received {}".format(response.status_code)
+	assert response.name==pet_name, "Expected pet name to be {} but received {}".format(pet_name,response.name)
 
